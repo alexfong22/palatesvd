6737be3a133202e04d32ba7227e6d2ed397e18ef
alexfong22 <alexanderfong22@gmail.com>
2015-12-25 16:58:34 -0600

    to run on '0.0.0.0'

--- app.py
+++ app.py
@@ -265,9 +265,7 @@ def recommendgo():
     
     
     
-    
-app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
 
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run('0.0.0.0')
